909bf94a7e7b04b546cc61fed2faec94081f1f1d
Richard O'Dwyer <117323203@umail.ucc.ie>
2018-02-09 12:25:59 +0000

    ebus deployment

---
+++ cost_calc.js
@@ -0,0 +1,32 @@
+/* global $ */
+
+function calcSub(){
+
+var argSubTotal;
+
+if(document.getElementByID('salesforce').checked){
+    argSubTotal = 100;
+}
+else{
+    argSubTotal = 300;
+}
+
+display(argSubTotal);
+}
+
+function display(parm1){
+    
+    document.getElementByID("subtotal").value = parm1;
+    document.getElementByID("Total").value = parm1;
+    
+    enablebtnProceed();
+}
+
+
+function enablebtnProceed(){
+    $('#btnProceed').prop('disabled', false);
+}
+
+function disablebtnProceed(){
+    $('#btnProceed').prop('disabled', true);
+}
