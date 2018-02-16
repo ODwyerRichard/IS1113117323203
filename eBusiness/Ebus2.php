a65566623af8715ed6eb95e298c2e1fb04768943
Richard O'Dwyer <117323203@umail.ucc.ie>
2018-02-16 11:23:01 +0000

    menu bar edit

--- eBusiness/Ebus2.php
+++ eBusiness/Ebus2.php
@@ -1,3 +1,8 @@
+<?php
+// start the session
+session_start();
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -22,6 +27,11 @@
             <br/>
             <button onClick="validateDetails()">Validate</button>
             
+            <?php
+            // set session variables
+            $_SESSION["total"] = $_POST["total"];
+            ?>
+       
         </div>
         
         <script type="text/javascript" src="ebus2_validator.js"></script>
